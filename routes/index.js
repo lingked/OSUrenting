c0d2a2f43ff37325f8c1eb48b81a628a9000d52a
LingkaiZhao <lingkaizhaolkz@gmial.com>
2020-02-14 23:58:52 +0000

    new

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.post("/register", function(req, res){
 			return res.render("register");
 		}
 		passport.authenticate("local")(req, res, function(){
-			req.flash("success", "Welcome to YelpCamp " + user.username);
+			req.flash("success", "Welcome to OSU Renting " + user.username);
 			res.redirect("/apartments");
 		});
 	});
